58c474e87fda5f8893fc52d326375ec5937fca9d
Mouad <devmahfoud@gmail.com>
2019-09-23 14:52:18 +0100

    manage authentication in front end

--- API/config/bundles.php
+++ API/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
